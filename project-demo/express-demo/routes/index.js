0e90c082dd7828e8621afc43dda4ef3e920fd341
wangyongtan <1345903768@qq.com>
2018-09-14 10:21:27 +0800

    change some code

--- project-demo/express-demo/routes/index.js
+++ project-demo/express-demo/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express-demo' ,message:"这是index.js中的信息数据"});
 });
 
 
