496b19df74f1ab367d78823701dd72caf948efe4
Wang YongTan <wangyt01@inspur.com>
2019-09-28 17:10:34 +0800

    '升级a8'

--- project-demo/my-app/src/app/modules/three-demo/three-demo.component.ts
+++ project-demo/my-app/src/app/modules/three-demo/three-demo.component.ts
@@ -1,6 +1,13 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-07-05 15:10:36
+ * @LastEditTime: 2019-08-19 09:44:55
+ * @LastEditors: Please set LastEditors
+ */
 import { Component, OnInit } from '@angular/core';
-import { Scene } from 'three';
-import * as THREE from 'three';
+// import { Scene } from 'three';
+// import * as THREE from 'three';
 
 @Component({
   selector: 'app-three-demo',
